3c4f2ffe3e7bd5311aea768c6222e8e02d690669
Belinda Crichlow <crichlowb@gmail.com>
2022-10-13 20:59:12 -0400

    files updated

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
-
 const form = document.getElementById('task-form');
 const taskName = document.getElementById('taskName');
 const assignedTo = document.getElementById('assignedTo');
 const taskDesc = document.getElementById('taskDesc');
-const dueDate = document.getElementById('dueDate"');
+const dueDate = document.getElementById('dueDate');
+const submitTask = document.getElementById('submitTask');
+const taskList = document.getElementById('taskList')
 
+
+//FORM VALIDATION
 //showinput errors
 function showError(input, message){
 	const formGroup = input.parentElement;
@@ -29,4 +32,39 @@ form.addEventListener('submit', function(e){
 	} else {
 		showSuccess(taskName);
 	}
+});
+
+
+
+//ADD TASKS TO THE TASK LIST
+submitTask.addEventListener('click',function(){
+  let listItem = document.createElement('li');
+   listItem.innerHTML = `
+     <div><h4>Task<h4>: ${taskName.value}</div>
+      <div><h4>Assigned</h4> To: ${assignedTo.value}</div>
+       <div><h4>Task Desc</h4>: ${taskDesc.value}</div>
+      <div><h4>Due Date</h4>: ${dueDate.value}</div>
+      <div class="pending">Status: Pending</div>
+      `;
+  taskList.appendChild(listItem);
+  
+  taskName.value = '';
+  assignedTo.value = '';
+  taskDesc.value = '';
+  dueDate.value = '';
+
+  /*
+	///add a line throuh the test
+	listItem.addEventListener('click', function(){ 		        		
+    paragraph.style.textDecoration = 'line-through';
+																 
+	});*/
+	
+//delete task
+	listItem.addEventListener('dblclick', function(){ 		        		
+    taskList.removeChild(listItem);
+																 
+	});
+  
+  
 })
--- js/taskManager.js
+++ js/taskManager.js
@@ -2,13 +2,12 @@
 //Create the task class
 class TaskManager {
   constructor(taskName, taskDesc, assignedTo, dueDate){
-      this.tasks= [];
       this.taskName = taskName;
       this.taskDesc = taskDesc;
       this.assignedTo = assignedTo;
       this.dueDate = dueDate;
+      
   }
-  
 };
 
 class UI {
@@ -17,105 +16,14 @@ class UI {
         // Create tr element
         const listItem = document.createElement('li');
         // Insert cols
-      listItem.innerHTML = `
-        
-      <h1>Task ${task.index}: ${task.taskName}</h1>
-      <p>Assigned To: ${task.taskDesc}</p>
-      <p>Description <br> ${task.assignedTo}</p>
-      <p>Due Date: ${task.dueDate}</p>
+        listItem.innerHTML = `
+        <div>${task.taskName}</div>
+      <div>${task.taskDesc}</div>
+      <div>${task.assignedTo}</div>
+      <div>${task.dueDate}</div>
 
       <div><a href="#" class="delete">X<a></div>`;
   
         taskList.appendChild(listItem);
     }
 }
-
-//Alert Section
-
-//Delete Section
-  /*deleteTask(target) {
-    if(target.className === 'delete') {
-      target.parentElement.parentElement.remove();
-    }
-}
-  //
-  clearFields() {
-    document.getElementById('taskName').value = '';
-    document.getElementById('taskDesc').value = '';
-    document.getElementById('assignedTo').value = '';
-    document.getElementById('dueDate').value = '';
-  }
-}*/
-
-//Local Storage Class
-class Store {
-  static getTasks() {
-    let tasks;
-    if(localStorage.getItem('tasks') === null) {
-      tasks = [];
-    } else {
-      tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
-
-    return tasks;
-  }
-   static removeTasks(assignedTo) {
-    const tasks = Store.getTasks();
-
-    tasks.forEach(function(tasks, index){
-     if(tasks.assignedTo === assignedTo) {
-      tasks.splice(index, 1);
-     }
-    });
-
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }
-}
-
-  //EventListener  DOMLoader event
-  document.addEventListener('DOMContentLoaded',     
-  Store.displayTasks);
-
-
-  // Event Listener for add task
-document.getElementById('task-form').addEventListener('submit', function(e){
-  
-  // Get form values
-  const taskName = document.getElementById('taskName').value;
-  const taskDesc = document.getElementById('taskDesc ').value;
-  const assignedTo = document.getElementById('assignedTo').value;
-  const dueDate = document.getElementById('dueDate').value;
-
-  // Instantiate task
-  //New task manager instance; all things inherited from task manager
-  const task = new TaskManager(taskName, taskDesc, assignedTo, dueDate);
-
-  // Instantiate task UI
-  const ui = new UI();
-
-  console.log(ui);
-
-  // Validate alert sectionn
-  if(taskName === '' || taskDesc === '' || assignedTo === '' || dueDate === '') {
-    
-    // Error alert
-    ui.showAlert('Please fill in all fields', 'error');
-  } else {
-    
-    // Add task to list
-    ui.addTaskToList(task);
-
-    // Add to LS
-    Store.addTask(task);
-
-    // Show success
-    ui.showAlert('Task Added!', 'success');
-  
-    // Clear fields
-    ui.clearFields();
-  }
-
-  e.preventDefault();
-});
-
-// Event Listener for delete
